0f83599427a34de1be334a68adfdc68879e0efbc
Guilherme Duarte <guilhermeduarte00@hotmail.com>
2024-05-05 23:17:42 +0100

    Exercise 3

--- group_project2/build_phylotree.py
+++ group_project2/build_phylotree.py
@@ -1,62 +1,55 @@
-import sys
-import requests
-import pprint  # to remove later - pretty print for json objects
+from requests import get, post
+from sys import argv
+from time import sleep
 
-from Bio.Blast import NCBIWWW, NCBIXML
 from Bio import SeqIO
-# from Bio import Entrez
+from Bio.Blast import NCBIWWW, NCBIXML
 
-UNIPROT_URL = 'https://rest.uniprot.org/uniprotkb/search?query='
+UNIPROT_URL = 'https://rest.uniprot.org/uniprotkb/'
+EBI_URL = 'https://www.ebi.ac.uk/Tools/services/rest/clustalo'
 MAX_SPECIES = 10
 SEQUENCE_FASTA_FILE = 'sequence.fasta'
 TOP_SEQUENCE_FASTA_FILE = 'sequences_to_analyse.fasta'
+ALIGNMENT_FILE = 'alignment.txt'
 
-# def print_sequence_info(seq_info):
-#     print(f'--- Sequence Info ---')
-#     uniprot_id = seq_info['results'][0]['uniProtkbId']
-#     print(f'UniProtKB ID: {uniprot_id}')
-#     first_pubic_date = seq_info['results'][0]['entryAudit']['firstPublicDate']
-#     print(f'First Public Date: {first_pubic_date}')
-#     organism = seq_info['results'][0]['organism']['scientificName']
-#     print(f'Organism: {organism}')
-#     recommended_name = seq_info['results'][0]['proteinDescription']['recommendedName']['fullName']['value']
-#     print(f'Recommended Name: {recommended_name}')
-#     gene_name = seq_info['results'][0]['genes'][0]['geneName']['value']
-#    print(f'Gene Name: {gene_name}')
+
+def do_request(method, url, command=None, parameters=None):
+    response = None
+    if method == 'get':
+        response = get(url+command, data=parameters)
+    elif method == 'post':
+        response = post(url + command, data=parameters)
+
+    if response.status_code == 200:
+        return response
+    else:
+        print(f'\t> Request failed with status code {response.status_code}')
+        print(f'\t> Response:\n{response.text}\n', response.text)
+        return None
+
 
 class Phylogenetics:
     def __init__(self, seq_id):
         self.seq_id = seq_id
         self.seq_info = None
 
-    def _fetch_UniProt(self):
-        try:
-            query = UNIPROT_URL + self.seq_id
-            response = requests.get(query)
-            results = response.json()
-            sequence = results['results'][0]
-            return sequence
-        except:
-            return None
-
     @staticmethod
     def _save_sequence_fasta(seq_ids, sequences, output_file):
         print(f'\t> Saving sequence{"s" if len(seq_ids) > 1 else ""} in {output_file}')
         with open(output_file, 'w') as f:
             for seq_id, sequence in zip(seq_ids, sequences):
-                f.write('>' + seq_id + '\n')
-                f.write(sequence + '\n')
+                f.write(f'>{seq_id.replace(" ", "-")}\n{sequence}\n')
 
     def data_collection(self):
-        seq_info = self._fetch_UniProt()
-        if not seq_info:
-            print(f'> No results found with the sequence ID {seq_id}')
-        else:
-            self.seq_info = seq_info
-            uniProt_id = self.seq_info['uniProtkbId']
-            print(f'> Found match on UniProt database with ID {uniProt_id}')
+        try:
+            results = do_request('get', UNIPROT_URL, f'/search?query={self.seq_id}')
+            self.seq_info = results.json()['results'][0]
+            species = self.seq_info['organism']['scientificName']
+            print(f'> Found match on UniProt database for species {species}')
             sequence = self.seq_info['sequence']['value']
-            self._save_sequence_fasta([uniProt_id], [sequence], SEQUENCE_FASTA_FILE)
+            self._save_sequence_fasta([species], [sequence], SEQUENCE_FASTA_FILE)
+        except:
+            print(f'> No results found with the sequence ID {seq_id}')
 
     def BLAST_analysis(self):
         print('> Performing a BLAST search')
@@ -95,11 +88,46 @@ class Phylogenetics:
         best_species_sequence = [species_sequence[species] for species in best_species]
         self._save_sequence_fasta(best_species, best_species_sequence, TOP_SEQUENCE_FASTA_FILE)
 
+    @staticmethod
+    def multiple_sequence_alignment():
+        print('> Applying Multiple Sequence Alignment')
+        sequence = SeqIO.read(SEQUENCE_FASTA_FILE, 'fasta')
+        species = list(SeqIO.parse(TOP_SEQUENCE_FASTA_FILE, 'fasta'))
+
+        run_response = do_request('post', EBI_URL, '/run', parameters={
+            'email': 'abc@abc.com',
+            'sequence': '\n'.join(f'>{seq_rec.name}\n{seq_rec.seq}' for seq_rec in [sequence] + species)
+        })
+        if not run_response:
+            return
+        job_id = run_response.text
+        print(f'\t> Job successfully submitted with ID {job_id}')
+
+        print(f'\t> Waiting for result...')
+        while True:
+            status_response = do_request('get', EBI_URL, f'/status/{job_id}')
+            if not status_response:
+                return
+            status = status_response.text
+            if status == 'FINISHED':
+                break
+            sleep(1)
+        print(f'\t> Job done')
+
+        result_response = do_request('get', EBI_URL, f'/result/{job_id}/aln-clustal_num')
+        if not result_response:
+            return
+        result = result_response.text
+
+        with open(ALIGNMENT_FILE, "w") as f:
+            f.write(result)
+        print(f'\t> Result saved on {ALIGNMENT_FILE}')
+
 
 if __name__ == '__main__':
-    # seq_id = sys.argv[1]
-    seq_id = 'P68871'
+    seq_id = argv[1]
 
     phylogenetics = Phylogenetics(seq_id)
     phylogenetics.data_collection()
     phylogenetics.BLAST_analysis()
+    phylogenetics.multiple_sequence_alignment()
